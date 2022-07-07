a4a305a4e35352548faaa286408448974b15273f
Paulo Teixeira <paulo.teixeira@fortbrasil.com.br>
2022-07-07 16:22:24 -0300

    Implementation get method all courses.

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
-
+import { COURSES } from './courses.mock';
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+  courses = COURSES;
+
+  getCourses(): Promise<any> {
+    return new Promise((resolve) => {
+      resolve(this.courses);
+    });
+  }
+}
